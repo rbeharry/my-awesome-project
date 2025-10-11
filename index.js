4237626fa03cd196492aad6e9704dfa21f224697
rbeharry <rlbeharry@hotmail.com>
2025-10-11 17:40:37 -0400

    Added back typewriter.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
-
 new Typewriter("#typewriter", {
-  strings: "Front-end developer + communications specialist",
+  strings: "Front end developer + communications specialist",
   autoStart: true,
-    cursor: ''
  
 });
+
 
 // Example usage:
 // const myElement = document.getElementById('myDiv');
