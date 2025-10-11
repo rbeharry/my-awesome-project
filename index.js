ecfb31b76cd8a62abcc0066b5327f24d1025c1b7
rbeharry <rlbeharry@hotmail.com>
2025-10-11 15:04:11 -0400

    Added JavaScript to front page

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 
-alert("Welcome! Enter your name in the box below for a personalized greeting!");
-
-function searchName(event) {
-  event.preventDefault();
-  let searchElement = document.querySelector(".search-input");
-
-  let newElement = document.querySelector(".current-text-title");
- newElement.innerHTML = "Hello " + searchElement.value + ""+ ", welcome to my site!";
-}
-let newHeader = document.querySelector("form");
-newHeader.addEventListener("submit", searchName);
 
 new Typewriter("#typewriter", {
   strings: "Front-end developer + communications specialist",
   autoStart: true,
+ 
 });
 
 // Example usage:
